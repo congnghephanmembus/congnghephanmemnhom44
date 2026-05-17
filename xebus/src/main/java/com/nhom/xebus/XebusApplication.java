4ebfc8cf46958d8a88417d40a30f70d1cd0d48c0
Nightnonefine <hiepbmt7712@gmail.com>
2026-05-17 19:50:45 +0700

    Khoi tao project

---
+++ xebus/src/main/java/com/nhom/xebus/XebusApplication.java
@@ -0,0 +1,13 @@
+package com.nhom.xebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XebusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XebusApplication.class, args);
+	}
+
+}
---
+++ xebus/src/test/java/com/nhom/xebus/XebusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom.xebus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XebusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
